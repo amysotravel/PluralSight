c3878b41ba897e85442fdbf60ef8b4e22c90432e
amysotravel <schmamy@gmail.com>
2017-08-23 16:57:19 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -38,10 +38,22 @@
 
 // Triple level JSON
 
-var theData2 = '{"Jane":{"age":"29","degree":{"AAS":"VMI","BA":"UVA"}}, "Jim":{"age":"49","degree":"MA"}}';
-var myObj = JSON.parse(theData2);
-console.log(myObj);
+// var theData2 = '{"Jane":{"age":"29","degree":{"AAS":"VMI","BA":"UVA"}}, "Jim":{"age":"49","degree":"MA"}}';
+// var myObj = JSON.parse(theData2);
+// console.log(myObj);
 
-document.getElementById('message').innerHTML = myObj.Jane.degree.BA;
+// document.getElementById('message').innerHTML = myObj.Jane.degree.BA;
+
+var xhr = new XMLHttpRequest();
+
+xhr.open('GET','data.json',true);
+
+xhr.send();
+
+xhr.onreadystatechange = function() {
+	
+	console.log(xhr.readyState);
+	
+	} //end function
 
 
