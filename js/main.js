45be92ce7d802725b90c88ec784793f5d94eed9c
amysotravel <schmamy@gmail.com>
2017-08-27 19:46:13 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 // JavaScript Document
 
-var xhr = XMLHttpRequest();
-xhr.open('GET', "data.JSON", true);
+var xhr = new XMLHttpRequest();
+xhr.open('GET', "data.json", true);
 xhr.responseType = 'text';
 xhr.send();
 
+
 xhr.onload = function() {
-	
-	if(xhr.status=200){
-		myStuff = JSON.parse(xhr.responseText);
-		console.log(myStuff);
-		console.log(myStuff[0].first);
-	}//end of if 
-} //end of function
+    if(xhr.status === 200) {
+        var myStuff = JSON.parse(xhr.responseText);
+        console.log(myStuff);
+        
+    
+    } // end if
+} // end function
+
