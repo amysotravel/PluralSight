be3a8d916e882d1d316f06326b32b8f5d4478449
amysotravel <schmamy@gmail.com>
2017-08-28 16:46:14 -0600

    Add files via upload

--- JSON4c/js/main.js
+++ JSON4c/js/main.js
@@ -27,7 +27,7 @@ function display(x){
 	details = "";
 	for (i=0;i<hotelInfo[x].details.length;i++){
 	console.log(hotelInfo[x].details[i]);
-	details += "<p>"hotelInfo[x].details[i]"</p>";
+	details += "<p>"+hotelInfo[x].details[i]+"</p>";
 	}//end loop
 document.getElementById('details').innerHTML = details;
 	
