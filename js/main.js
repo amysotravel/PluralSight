cc0cc710f09370c329d1e192be7ad0fdcf2886b6
amysotravel <schmamy@gmail.com>
2017-08-27 19:43:32 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -10,6 +10,6 @@ xhr.onload = function() {
 	if(xhr.status=200){
 		myStuff = JSON.parse(xhr.responseText);
 		console.log(myStuff);
-		
+		console.log(myStuff[0].first);
 	}//end of if 
 } //end of function
