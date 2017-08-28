ac384102a91c01aad832d261a2c133117cef6f1d
amysotravel <schmamy@gmail.com>
2017-08-27 19:35:45 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,71 +1,15 @@
 // JavaScript Document
 
-// Exploring JSON
-
-// var start = '{"favColor":"Blue", "favSeason":"Summer"}';
-
-// var myObject = JSON.parse(start);
-// console.log(myObject);
-
-// var myString = JSON.stringify(myObject);
-// console.log(myString);
-
-// var myObject = JSON.parse(myString);
-// console.log(myObject);
-
-
-/////////////////////////////////////////////////
-
-// Single level JSON 
-
-// var theData1 = '{"first":"Paul", "last":"Cheney", "city":"Salem"}';
-// var myObj = JSON.parse(theData1);
-// console.log(myObj);
-
-// document.getElementById('message').innerHTML = myObj.city;
-
-///////////////////////////////////////////////////
-
-// Double level JSON
-
-// var theData2 = '{"Jane":{"age":"29","degree":"MFA"}, "Jim":{"age":"49","degree":"MA"}}';
-// var myObj = JSON.parse(theData2);
-// console.log(myObj);
-
-// document.getElementById('message').innerHTML = myObj.Jane.age;
-
-///////////////////////////////////////////////////
-
-// Triple level JSON
-
-// var theData2 = '{"Jane":{"age":"29","degree":{"AAS":"VMI","BA":"UVA"}}, "Jim":{"age":"49","degree":"MA"}}';
-// var myObj = JSON.parse(theData2);
-// console.log(myObj);
-
-// document.getElementById('message').innerHTML = myObj.Jane.degree.BA;
-
-var xhr = new XMLHttpRequest();
-
-xhr.open('GET','data.json',true);
+var xhr = XMLHttpRequest();
+xhr.open('GET, "data.JSON", true);
 xhr.responseType = 'text';
-
-
-// xhr.onreadystatechange = function() {
-	
-// 	console.log(xhr.readyState);
-// 	console.log(xhr.status);
-// 	console.log(xhr.statusText);
-	
-// 	}; //end function
-	
-	
-	xhr.onload = function (){
-		if(xhr.status===200){ 
-		var myStuff = JSON.parse(xhr.responseText);
-		console.log(myStuff);
-		} // end if
-		}; // end function
-
-
 xhr.send();
 
+xhr.onload = function() {
+	
+	if(xhr.status=200){
+		myStuff = JSON.parse(xhr.responseText);
+		console.log(myStuff);
+		
+	}//end of if 
+} //end of function
