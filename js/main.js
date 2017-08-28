1ab750b034ec9c045e6cbffd40e5b399b5c78e66
amysotravel <schmamy@gmail.com>
2017-08-27 20:28:16 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 // JavaScript Document
-
+var hotelInfo;
+var details;
 var xhr = new XMLHttpRequest();
 xhr.open('GET', "data.json", true);
 xhr.responseType = 'text';
@@ -8,25 +9,8 @@ xhr.send();
 
 xhr.onload = function() {
     if(xhr.status === 200) {
-        var myStuff = JSON.parse(xhr.responseText);
-        console.log(myStuff);
-        
-		for(i=0; i<myStuff.presidents.length;i++){
-			console.log(myStuff.presidents[i].first);
-					console.log(myStuff.presidents[i].last);
-		}//end loop
-    
-		var myString = "";
-		for(i=0; i<myStuff.presidents.length; i++){
-			
-			var x=i+1;
-			myString += "President" + x + "was";
-			myString += myStuff.presidents[i].first + " ";
-			myString += myStuff.presidents[i].last + " ";
-		}// end loop
-		
-		document.getElementById('message').innerHTML = myString;
-		
+        hotelInfo = JSON.parse(xhr.responseText);
+        console.log(hotelInfo);
+  
     } // end if
 } // end function
-
