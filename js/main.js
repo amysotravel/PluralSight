ba89739deeddfd1725309db537c1d91ccf839217
amysotravel <schmamy@gmail.com>
2017-08-27 20:07:54 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -16,6 +16,17 @@ xhr.onload = function() {
 					console.log(myStuff.presidents[i].last);
 		}//end loop
     
+		var myString = "";
+		for(i=0; i<myStuff.presidents.length; i++){
+			
+			var x=i+1;
+			myString += "President" + x + "was";
+			myString += myStuff.presidents[i].first + " ";
+			myString += myStuff.presidents[i].last + " ";
+		}// end loop
+		
+		document.getElementById('message').innerHTML = myString;
+		
     } // end if
 } // end function
 
