609fba2759c3ff57006e627cf4c1c8dade811e12
amysotravel <schmamy@gmail.com>
2017-08-27 21:02:26 -0600

    Add files via upload

--- JSON4c/js/main.js
+++ JSON4c/js/main.js
@@ -11,7 +11,7 @@ xhr.onload = function() {
     if(xhr.status === 200) {
         hotelInfo = JSON.parse(xhr.responseText);
         console.log(hotelInfo);
-  
+  display(0);
     } // end if
 } // end function
 
@@ -27,7 +27,7 @@ function display(x){
 	details = "";
 	for (i=0;i<hotelInfo[x].details.length;i++){
 		console.log(hotelInfo[x].details[i]);
-		details+=hotelInfo[x].details[i];
+		details+= "<p>"hotelInfo[x].details[i]"</p>";
 	}//end loop
 	document.getElementById('details').innerHTML = details;
 	
